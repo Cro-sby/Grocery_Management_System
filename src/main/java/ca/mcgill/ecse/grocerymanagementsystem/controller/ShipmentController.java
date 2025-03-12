14440f4b2ccf62f4fe1151b5d831fe6010d98d93
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-03-12 14:53:18 -0400

    Add skeletons for remaining controller methods

---
+++ src/main/java/ca/mcgill/ecse/grocerymanagementsystem/controller/ShipmentController.java
@@ -0,0 +1,19 @@
+package ca.mcgill.ecse.grocerymanagementsystem.controller;
+
+public class ShipmentController {
+	public static void createShipment() {
+		throw new RuntimeException("TODO");
+	}
+
+	public static void deleteShipment(int shipmentNumber) {
+		throw new RuntimeException("TODO");
+	}
+
+	public static void addItemToShipment(int shipmentNumber, String itemName) {
+		throw new RuntimeException("TODO");
+	}
+
+	public static void updateQuantityInShipment(int shipmentNumber, String itemName, int newQuantity) {
+		throw new RuntimeException("TODO");
+	}
+}
