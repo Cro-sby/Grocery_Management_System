6d5675697e643f4e2fec33ddd2680011cb3e1b8c
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-03-19 17:27:26 -0400

    Update controller API

---
+++ src/main/java/ca/mcgill/ecse/grocerymanagementsystem/controller/OrderProcessingController.java
@@ -0,0 +1,27 @@
+package ca.mcgill.ecse.grocerymanagementsystem.controller;
+
+public class OrderProcessingController {
+	public static int calculateTotalOrderCost(int orderNumber, int usePoints) {
+		throw new RuntimeException("TODO");
+	}
+
+	public static void payForOrder(int orderNumber, boolean usePoints) {
+		throw new RuntimeException("TODO");
+	}
+
+	public static void assignOrderToEmployee(int orderNumber, String employeeUsername) {
+		throw new RuntimeException("TODO");
+	}
+
+	public static void finishOrderAssembly(int orderNumber) {
+		throw new RuntimeException("TODO");
+	}
+
+	public static void deliverOrder(int orderNumber) {
+		throw new RuntimeException("TODO");
+	}
+
+	public static void cancelOrder(int orderNumber) {
+		throw new RuntimeException("TODO");
+	}
+}
