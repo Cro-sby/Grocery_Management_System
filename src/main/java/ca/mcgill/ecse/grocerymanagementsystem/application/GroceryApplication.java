db765344f6dcc6ea3f227cdb95c52c44c0fbeba2
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-04-09 18:30:15 -0400

    Use GroceryStoreGUIApplication instead of GroceryApplication

--- src/main/java/ca/mcgill/ecse/grocerymanagementsystem/application/GroceryApplication.java
+++ src/main/java/ca/mcgill/ecse/grocerymanagementsystem/application/GroceryApplication.java
@@ -1,10 +1,7 @@
 package ca.mcgill.ecse.grocerymanagementsystem.application;
 
-import ca.mcgill.ecse.grocerymanagementsystem.view.GroceryStoreView;
-import javafx.application.Application;
-
 public class GroceryApplication {
 	public static void main(String[] args) {
-		Application.launch(GroceryStoreView.class, args);
+		System.out.println("Hello there!");
 	}
 }
